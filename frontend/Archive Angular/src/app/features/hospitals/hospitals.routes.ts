cb5c2d8ca7a8699392e0415d26a8f0c25e9fa91e
Siva Prabhaharan Jayakumar <sivaprabhaharan.j@gmail.com>
2026-02-13 13:56:25 +0530

    feat: migrate angular to react (phases 2-4) with fambot agent

--- frontend/Archive Angular/src/app/features/hospitals/hospitals.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { HospitalsComponent } from './hospitals.component';
-
-export const hospitalsRoutes: Routes = [
-  {
-    path: '',
-    component: HospitalsComponent
-  }
-];
--- frontend/Archive Angular/src/app/features/medical-records/medical-records.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-
-export const medicalRecordsRoutes: Routes = [
-  {
-    path: '',
-    loadComponent: () => import('./medical-records.component').then(m => m.MedicalRecordsComponent)
-  }
-];
